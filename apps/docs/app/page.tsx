f6a7cbad321f3f18190e57977c31cce94c3729a3
TharindaPrabhath <tharindahp@gmail.com>
2024-03-27 22:56:57 +0530

    first commit

---
+++ apps/docs/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@repo/ui/components/ui/button";
+
+export default function Page() {
+  return (
+    <main>
+      <Button variant="outline">Click me</Button>
+      <p className="text-2xl">Text</p>
+    </main>
+  );
+}
