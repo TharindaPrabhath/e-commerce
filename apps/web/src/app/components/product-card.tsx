095c4c8f3b36b3823732f7113fa50286cb373470
TharindaPrabhath <tharindahp@gmail.com>
2024-04-05 20:10:29 +0530

    ui changes

--- apps/web/src/app/components/product-card.tsx
+++ apps/web/src/app/components/product-card.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState } from "react";
+
 import Image from "next/image";
 import Link from "next/link";
 
@@ -10,28 +12,49 @@ import {
   TooltipTrigger,
 } from "@pulzeup/ui/components/ui/tooltip";
 import { Typography } from "@pulzeup/ui/components/ui/typography";
-import { useState } from "react";
 
-function ProductCard() {
-  const first =
-    "https://incarnage.com/cdn/shop/files/DSC04169.jpg?v=1701282128&width=650";
-  const second =
-    "https://incarnage.com/cdn/shop/files/DSC03801_f4bba734-7f94-411c-9012-c4bfe9127d7c.jpg?v=1706780282&width=650";
-  const [thumbnail, setThumbnail] = useState(first);
+import { DEFAULT_FALLBACK_IMAGE } from "@/src/const";
+
+type ProductCardProps = {
+  data: {
+    name: string;
+    price: number;
+    thumbnail: string;
+    variants?: {
+      image: string;
+      text: string;
+    }[];
+  };
+};
+
+function ProductCard({ data }: ProductCardProps) {
+  const [thumbnail, setThumbnail] = useState<string | undefined>(
+    data.thumbnail,
+  );
+  const hasVariants = data.variants && data?.variants?.length > 1;
+
+  const getTransitionImage = () => {
+    if (!hasVariants) return data.thumbnail;
+
+    if (thumbnail === data.variants?.[0]?.image) {
+      if (data?.variants?.length! > 1) return data.variants?.[1]?.image;
+    }
+    return data.variants?.[0]?.image;
+  };
 
   return (
-    <div className="w-72">
+    <div className="w-fit md:w-72">
       <Link href="/">
         <Image
-          src={thumbnail}
+          src={thumbnail ?? DEFAULT_FALLBACK_IMAGE}
           alt="Product thumbnail"
           width={300}
           height={300}
           onMouseEnter={(e) => {
-            setThumbnail(second);
+            if (hasVariants) setThumbnail(getTransitionImage());
           }}
           onMouseLeave={(e) => {
-            setThumbnail(first);
+            if (hasVariants) setThumbnail(thumbnail);
           }}
         />
         <Typography variant="p" className="font-semibold mt-2">
@@ -40,16 +63,16 @@ function ProductCard() {
       </Link>
 
       <Typography>LKR 1500</Typography>
-      <div className="flex flex-row items-center gap-2 mt-4">
-        <ProductVariant
-          data={{ image: first, text: "Space Black" }}
-          onClick={setThumbnail}
-        />
-        <ProductVariant
-          data={{ image: second, text: "Sky Blue" }}
-          onClick={setThumbnail}
-        />
-      </div>
+      {hasVariants && (
+        <div className="flex flex-row items-center gap-2 mt-4">
+          {data.variants?.map((variant) => (
+            <ProductVariant
+              data={{ image: variant.image, text: variant.text }}
+              onClick={setThumbnail}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,20 +1,44 @@
-import { Button } from "@pulzeup/ui/components/ui/button";
 import { Typography } from "@pulzeup/ui/components/ui/typography";
 import ProductCard from "./components/product-card";
 
 export default function Page() {
   return (
     <main className="container">
-      <section>
+      <section className="py-4">
         <Typography variant="h3">Featured Items</Typography>
-        <div className="grid grid-cols-2 md:grid-flow-col md:w-fit gap-4">
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+        <div className="mt-4 grid grid-cols-2 md:grid-flow-col md:w-fit gap-x-4 gap-y-10">
+          {Products.map((product) => (
+            <ProductCard data={product} />
+          ))}
         </div>
       </section>
     </main>
   );
 }
+
+const Products = [
+  {
+    name: "Polo T shirt",
+    price: 1500,
+    thumbnail:
+      "https://incarnage.com/cdn/shop/files/DSC04169.jpg?v=1701282128&width=650",
+  },
+  {
+    name: "Incarnage T shirt",
+    price: 2700,
+    thumbnail:
+      "https://incarnage.com/cdn/shop/files/DSC04169.jpg?v=1701282128&width=650",
+    variants: [
+      {
+        image:
+          "https://incarnage.com/cdn/shop/files/DSC04169.jpg?v=1701282128&width=650",
+        text: "Space Black",
+      },
+      {
+        image:
+          "https://incarnage.com/cdn/shop/files/DSC03801_f4bba734-7f94-411c-9012-c4bfe9127d7c.jpg?v=1706780282&width=650",
+        text: "Sky Blue",
+      },
+    ],
+  },
+];
